8ee5b94b80fed18a76c52d78454f20c4d3aaad7f
schbrongx <pesendorfer@was.ch>
2019-07-09 11:53:05 +0200

    toi.py: now able to load items and categories from config-file

--- src/toi.py
+++ src/toi.py
@@ -1,49 +1,41 @@
 #!/usr/bin/env python3
 """ An item-overlay for Teamfight Tactics. """
 
-import tkinter as tk
 import os
+import tkinter as tk
+import configparser
 
 
 PATH = os.path.dirname(os.path.abspath(__file__)) + "/.."
 DIR_IMG_ITEMS = PATH + "/images/items"
+CONFIG = PATH + "/toi.conf"
 print("DIR_IMG_ITEMS: %s" % DIR_IMG_ITEMS)
+print("CONFIG: %s" % CONFIG)
 
 
 class Toi:
     """ A container class for the TOI window. """
-    def __init__(self, item_images_path):
-        self.items = {
-            "b_f_sword": {
-                "icon": "b_f_sword.png",
-                "text": "text for b. f. sword",
-            },
-            "blade_of_the_ruined_king": {
-                "icon": "blade_of_the_ruinged_king.png",
-                "text": "text for blade of the ruined king",
-            },
-            "chain_vest": {
-                "icon": "chain_vest.png",
-                "text": "text for chain vest",
-            },
-        }
+    def __init__(self, item_images_path, config_path):
+        # load config from config_path
+        self.config = configparser.ConfigParser(interpolation=None)
+        self.config.read(config_path)
 
-        self.categories = {
-            "b_f_sword": {
-                "mainitem": "b_f_sword",
-                "subitems": [
-                    "blade_of_the_ruined_king",
-                    "chain_vest",
-                ]
-            },
-            "chain_vest": {
-                "mainitem": "chain_vest",
-                "subitems": [
-                    "b_f_sword",
-                    "blade_of_the_ruined_king",
-                ]
-            },
-        }
+        # load all items from config [GLOBAL]itemlist
+        self.items = {}
+        for _i in self.config.get("GLOBAL", "itemlist", fallback=None).split(", "):
+            self.items[_i] = {
+                "name": self.config.get(_i, "name", fallback="missing name"),
+                "icon": self.config.get(_i, "icon", fallback="missing_image.png"),
+                "text": self.config.get(_i, "text", fallback="missing text")
+            }
+
+        self.categories = {}
+        for _c in self.config.get("GLOBAL", "categorylist", fallback=None).split(", "):
+            self.categories[_c] = {
+                "mainitem": self.config.get(_c, "mainitem", fallback="missing main item"),
+                "subitems": self.config.get(_c, "subitems", fallback="missing subitems").split(", ")
+            }
+
         # create tkinter window
         self.root = tk.Tk()
 
@@ -53,7 +45,7 @@ class Toi:
         self.load_images()
 
         # create the buttons
-        self.category_buttons = []
+        self.buttons = []
         self.create_buttons()
 
         # enter Tk mainloop
@@ -63,34 +55,32 @@ class Toi:
         """ load images for all items into a dict. """
         print("Loading images from %s" % self.item_images_path)
         _flist = [f for f in os.listdir(self.item_images_path)]
-        for f in _flist:
-            self.item_images[f] = tk.PhotoImage(file=self.item_images_path + "/" + f)
+        for _f in _flist:
+            self.item_images[_f] = tk.PhotoImage(file=self.item_images_path + "/" + _f)
         print("Added %d images" % len(self.item_images))
 
     def create_buttons(self):
         """ create the buttons. """
         i = 0
-        for c_name, c_content in self.categories.items():
-            print("Category: %s" % c_name)
-            print("  Main-Item: %s" % c_content["mainitem"])
-            print("    Icon: %s" % self.items[c_content["mainitem"]]["icon"])
-            print("    Text: %s" % self.items[c_content["mainitem"]]["text"])
-            self.category_buttons.append(
-                tk.Button(self.root,
-                          text=self.items[c_content["mainitem"]]["text"],
-                          command=lambda s=self.items[c_content["mainitem"]]["icon"]: print(s)
-                         )
-            )
-            self.category_buttons[i].config(
-                image=self.item_images[self.items[c_content["mainitem"]]["icon"]],
+        for c_key, c_val in self.categories.items():
+            #c_name = c_val["mainitem"]
+            c_icon = self.items[c_val["mainitem"]]["icon"]
+            c_text = self.items[c_val["mainitem"]]["text"]
+            self.buttons.append(tk.Button(self.root, text=c_text))
+            # pylint: disable=cell-var-from-loop
+            self.buttons[i].config(
+                image=self.item_images[c_icon],
                 width=64,
-                height=64
+                height=64,
+                command=lambda s=c_icon: print(s)
             )
-            self.category_buttons[i].grid(column=i+1, row=1, sticky="n")
-            for i_name in c_content["subitems"]:
-                print("  Sub-Item: %s" % i_name)
-                print("    Icon: %s" % self.items[i_name]["icon"])
-                print("    Text: %s" % self.items[i_name]["text"])
+            # pylint: enable=cell-var-from-loop
+            self.buttons[i].grid(column=i+1, row=1, sticky="n")
+            for i_val in c_val["subitems"]:
+                s_name = i_val
+                s_icon = self.items[i_val]["icon"]
+                s_text = self.items[i_val]["text"]
+                #print("  Sub-Item: %s, Icon: %s, Text: %s" % (s_name, s_icon, s_text))
             i += 1
 
         #_icon = tk.PhotoImage(file=r"./images/items/b_f_sword.png")
@@ -106,4 +96,4 @@ class Toi:
         self.root.mainloop()
 
 if __name__ == "__main__":
-    TOIWINDOW = Toi(DIR_IMG_ITEMS)
+    TOIWINDOW = Toi(DIR_IMG_ITEMS, CONFIG)
